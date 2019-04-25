1604e97abde826d72491c815f25a55a95c3c43be
Kranando <41551796+Kranando@users.noreply.github.com>
2019-04-25 10:45:00 +0300

    Yes

--- sketch.js
+++ sketch.js
@@ -16,66 +16,72 @@ function setup() {
     height = 480;
     createCanvas(width,height);
     frameRate(60);
+    stroke(255);
     x = width/2;
     y = height/2;
-}
-
-function draw() {
-    background(0);
-    fill(0);
-    fuck();
-    fill(255);
-    move();
-    ball();
+    addBall();
+    addBall();
 }
 
 function addBall() {
     balls.push ({
-        ellipse(x,y,radius*2,radius*2)
+        id: balls.length,
+        x: Math.floor(Math.random() * width) + 0,
+        y: Math.floor(Math.random() * height) + 0,
+
+        radius: radius,
+
+        vx: vx,
+        vy: vy,
+
+        lx: 0,
+        ly: 0,
+
+        locked: false,
     })
+    console.log("Fuck");
 }
 
-function move() {
+function move(ball) {
     if (!locked) {
-        x += vx;
-        y += vy;
+        ball.x += ball.vx;
+        ball.y += ball.vy;
     }
-    if (y < height) {
-        vy += gravity;
+    if (ball.y < height) {
+        ball.vy += gravity;
     }
-    if (y + radius > height) {
-        y = height - radius;
+    if (ball.y + radius > height) {
+        ball.y = height - ball.radius;
         vy *= -bounceFactor;
     }
-    if (x + radius > width) {
-        x = width - radius;
-        vx *= -bounceFactor;
+    if (ball.x + ball.radius > width) {
+        ball.x = width - ball.radius;
+        ball.vx *= -bounceFactor;
     }
-    if (x < radius) {
-        x = radius;
-        vx *= -bounceFactor;
+    if (ball.x < ball.radius) {
+        ball.x = ball.radius;
+        ball.vx *= -bounceFactor;
     }
-    if (y < radius) {
-        y = radius;
-        vy *= - bounceFactor;
+    if (ball.y < ball.radius) {
+        ball.y = ball.radius;
+        ball.vy *= -bounceFactor;
     }
 }
 
-function fuck() {
-    ellipse(mouseX,mouseY,50,50);
-    var dist = Math.sqrt((mouseX - x) * (mouseX - x) + (mouseY - y) * (mouseY - y));
+function fuck(ball) {
+    var dist = Math.sqrt((mouseX - ball.x) * (mouseX - ball.x) + (mouseY - ball.y) * (mouseY - ball.y));
     //console.log(dist);
     if (mouseIsPressed) {
-        if (dist < radius + 50) {
+        if (dist < ball.radius * 2) {
             locked = true;
-            vx = 0;
-            vy = 0;
-            var lx = x;
-            var ly = y;
-            x = mouseX;
-            y = mouseY;
-            vx = x - lx;
-            vy = y - ly;
+            ball.vx = 0;
+            ball.vy = 0;
+            ball.lx = ball.x;
+            ball.ly = ball.y;
+            ball.x = mouseX;
+            ball.y = mouseY;
+            ball.vx = ball.x - ball.lx;
+            ball.vy = ball.y - ball.ly;
             gravity = 0;
         } else {
             gravity = 0.5;
@@ -84,4 +90,15 @@ function fuck() {
         gravity = 0.5;
         locked = false;
     }
+}
+
+function makeBall(ball) {
+    ellipse(ball.x,ball.y,ball.radius*2,ball.radius*2);
+}
+
+function draw() {
+    background(0);
+    balls.map(ball => makeBall(ball));
+    balls.map(ball => fuck(ball));
+    balls.map(ball => move(ball));
 }
